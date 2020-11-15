1e34a34fce3c4a9247ae9e0adc464470e681d57b
mceck <39463235+mceck@users.noreply.github.com>
2020-11-15 03:22:53 +0100

    add drawer

--- lib/widget/layout/drawer.dart
+++ lib/widget/layout/drawer.dart
@@ -1,4 +1,9 @@
+import 'package:bc4f/screens/groups/view/group-view.dart';
+import 'package:bc4f/screens/homepage/homepage.dart';
+import 'package:bc4f/screens/search/search.dart';
+import 'package:bc4f/screens/tags/view/tag-view.dart';
 import 'package:bc4f/service/firebase-service.dart';
+import 'package:bc4f/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class Bc4fDrawer extends StatelessWidget {
@@ -6,16 +11,52 @@ class Bc4fDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Drawer(
-        child: Column(
-          children: [
-            InkWell(
-              onTap: FirebaseService.logout,
-              child: ListTile(
-                leading: Icon(Icons.exit_to_app),
-                title: Text('Logout'),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: kDefaultPadding),
+          child: Column(
+            children: [
+              InkWell(
+                onTap: () => Navigator.of(context).pushNamedAndRemoveUntil(
+                    HomepageScreen.route, (route) => false),
+                child: ListTile(
+                  leading: Icon(Icons.home),
+                  title: Text('Home'),
+                ),
               ),
-            ),
-          ],
+              InkWell(
+                onTap: () => Navigator.of(context)
+                    .pushNamedAndRemoveUntil(GroupView.route, (route) => false),
+                child: ListTile(
+                  leading: Icon(Icons.group_work),
+                  title: Text('Groups'),
+                ),
+              ),
+              InkWell(
+                onTap: () => Navigator.of(context)
+                    .pushNamedAndRemoveUntil(TagView.route, (route) => false),
+                child: ListTile(
+                  leading: Icon(Icons.label),
+                  title: Text('Tags'),
+                ),
+              ),
+              InkWell(
+                onTap: () => Navigator.of(context).pushNamedAndRemoveUntil(
+                    SearchScreen.route, (route) => false),
+                child: ListTile(
+                  leading: Icon(Icons.search),
+                  title: Text('Search'),
+                ),
+              ),
+              Spacer(),
+              InkWell(
+                onTap: FirebaseService.logout,
+                child: ListTile(
+                  leading: Icon(Icons.exit_to_app),
+                  title: Text('Logout'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
